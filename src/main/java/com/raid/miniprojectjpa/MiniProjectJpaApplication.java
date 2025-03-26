ec68aa8a4108b58883dd74c8215bdeff4481f039
Raid Thabet <thabet.raid123@gmail.com>
2025-03-26 12:37:03 +0100

    Initial commit

---
+++ src/main/java/com/raid/miniprojectjpa/MiniProjectJpaApplication.java
@@ -0,0 +1,13 @@
+package com.raid.miniprojectjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniProjectJpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MiniProjectJpaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/raid/miniprojectjpa/MiniProjectJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raid.miniprojectjpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniProjectJpaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
