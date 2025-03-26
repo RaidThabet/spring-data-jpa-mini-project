ecb28c177eb2a4be4736b533f3c6c9c2c7d5b133
Raid Thabet <thabet.raid123@gmail.com>
2025-03-26 15:19:46 +0100

    Added Author entity

---
+++ src/main/java/com/raid/miniprojectjpa/models/Author.java
@@ -0,0 +1,81 @@
+package com.raid.miniprojectjpa.models;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.proxy.HibernateProxy;
+
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@Entity
+//@Table(name = "AUTHOR_TBL")
+public class Author {
+
+    // we use Integer instead of int because when hibernate sees that this id has a null value,
+    // it will automatically generate a new id and insert a new line into our table. Otherwise, it
+    // will just fetch for an author with an id of 0, because the default value for int primitive
+    // type is 0.
+    @Id
+    @GeneratedValue
+    // if we want to specify our own way of sequence generation type
+    /*@SequenceGenerator(
+            name = "author_sequence",
+            sequenceName = "author_sequence",
+            allocationSize = 1
+    )*/
+    /*@TableGenerator(
+            name = "author_id_gen",
+            table = "id_generator",
+            pkColumnName = "id_name",
+            pkColumnValue = "id_value",
+            allocationSize = 1
+    )*/
+    private Integer id;
+
+    @Column(
+            name = "f_name",
+            length = 35
+    )
+    private String firstName;
+
+    private String lastName;
+
+    @Column(
+            unique = true,
+            nullable = false
+    )
+    private String email;
+
+    private int age;
+
+    @Column(
+            updatable = false,
+            nullable = false
+    )
+    private LocalDateTime createdAt;
+
+    @Column(
+            insertable = false
+    )
+    private LocalDateTime modifiedAt;
+
+    @Override
+    public final boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null) return false;
+        Class<?> oEffectiveClass = o instanceof HibernateProxy ? ((HibernateProxy) o).getHibernateLazyInitializer().getPersistentClass() : o.getClass();
+        Class<?> thisEffectiveClass = this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass() : this.getClass();
+        if (thisEffectiveClass != oEffectiveClass) return false;
+        Author author = (Author) o;
+        return getId() != null && Objects.equals(getId(), author.getId());
+    }
+
+    @Override
+    public final int hashCode() {
+        return this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass().hashCode() : getClass().hashCode();
+    }
+}
