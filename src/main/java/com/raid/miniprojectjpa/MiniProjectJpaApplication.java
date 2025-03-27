5c098c99f0d14e83b7031112f1f32fb88a85d719
Raid Thabet <thabet.raid123@gmail.com>
2025-03-27 16:49:39 +0100

    Used Java Faker library to generate random data for the databas

--- src/main/java/com/raid/miniprojectjpa/MiniProjectJpaApplication.java
+++ src/main/java/com/raid/miniprojectjpa/MiniProjectJpaApplication.java
@@ -1,5 +1,7 @@
 package com.raid.miniprojectjpa;
 
+import com.github.javafaker.Faker;
+import com.raid.miniprojectjpa.models.Author;
 import com.raid.miniprojectjpa.models.Video;
 import com.raid.miniprojectjpa.repositories.AuthorRepository;
 import com.raid.miniprojectjpa.repositories.VideoRepository;
@@ -7,6 +9,8 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+
+import java.time.LocalDateTime;
 
 @SpringBootApplication
 public class MiniProjectJpaApplication {
@@ -16,27 +20,30 @@ public class MiniProjectJpaApplication {
     }
 
 
-
     @Bean
     public CommandLineRunner commandLineRunner(
             AuthorRepository repository, // will be automatically injected
             VideoRepository videoRepository) {
         return args -> {
-//            var author = Author.builder()
-//                    .firstName("Raid")
-//                    .lastName("Thabet")
-//                    .age(22)
-//                    .email("thabet@mail.com")
-//                    .createdAt(LocalDateTime.now())
+//            for (int i = 0; i < 50; i++) {
+//                Faker faker = new Faker();
+//                var author = Author.builder()
+//                        .firstName(faker.name().firstName())
+//                        .lastName(faker.name().lastName())
+//                        .age(faker.number().numberBetween(18, 50))
+//                        .email(faker.internet().emailAddress())
+//                        .createdAt(LocalDateTime.now())
+//                        .build();
+//
+//                repository.save(author);
+//
+//            }
+//            var video = Video.builder()
+//                    .name("abc")
+//                    .length(3)
 //                    .build();
 //
-//            repository.save(author);
-            var video = Video.builder()
-                    .name("abc")
-                    .length(3)
-                    .build();
-
-            videoRepository.save(video);
+//            videoRepository.save(video);
         };
     }
 
