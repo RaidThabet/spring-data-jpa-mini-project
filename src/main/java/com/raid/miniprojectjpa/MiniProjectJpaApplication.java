3083227f1982b16d8594f4a72b726e6f971f1bb5
Raid Thabet <thabet.raid123@gmail.com>
2025-03-27 17:10:22 +0100

    Used the @Query annotation to define custom queries

--- src/main/java/com/raid/miniprojectjpa/MiniProjectJpaApplication.java
+++ src/main/java/com/raid/miniprojectjpa/MiniProjectJpaApplication.java
@@ -25,19 +25,38 @@ public class MiniProjectJpaApplication {
             AuthorRepository repository, // will be automatically injected
             VideoRepository videoRepository) {
         return args -> {
-//            for (int i = 0; i < 50; i++) {
-//                Faker faker = new Faker();
-//                var author = Author.builder()
-//                        .firstName(faker.name().firstName())
-//                        .lastName(faker.name().lastName())
-//                        .age(faker.number().numberBetween(18, 50))
-//                        .email(faker.internet().emailAddress())
-//                        .createdAt(LocalDateTime.now())
-//                        .build();
-//
-//                repository.save(author);
-//
-//            }
+            Faker faker = new Faker();
+            for (int i = 0; i < 50; i++) {
+                var author = Author.builder()
+                        .firstName(faker.name().firstName())
+                        .lastName(faker.name().lastName())
+                        .age(faker.number().numberBetween(18, 50))
+                        .email(faker.internet().emailAddress())
+                        .createdAt(LocalDateTime.now())
+                        .build();
+
+                repository.save(author);
+
+            }
+
+            // Update author with id=1
+            var author = Author.builder()
+                    .id(1) // if the provided id does not exist, a new record will be inserted into the database
+                    .firstName("Raid")
+                    .lastName("Thabet")
+                    .age(22)
+                    .email("thabet@mail.com")
+                    .createdAt(LocalDateTime.now())
+                    .build();
+//            repository.save(author); // make the update
+
+
+            // update Author a set a.age = 20 where a.id = 1
+            repository.updateAuthor(24, 1);
+
+            repository.updateAllAuthorsAges(100);
+
+
 //            var video = Video.builder()
 //                    .name("abc")
 //                    .length(3)
--- src/main/java/com/raid/miniprojectjpa/repositories/AuthorRepository.java
+++ src/main/java/com/raid/miniprojectjpa/repositories/AuthorRepository.java
@@ -1,12 +1,27 @@
 package com.raid.miniprojectjpa.repositories;
 
 import com.raid.miniprojectjpa.models.Author;
+import jakarta.persistence.NamedQuery;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Collection;
 import java.util.List;
 
 public interface AuthorRepository extends JpaRepository<Author, Integer> {
+    @Modifying
+    @Transactional
+    @Query("update Author a set a.age = :age where a.id = :id")
+    int updateAuthor(@Param("age") int age2, @Param("id") int id);
+
+    @Modifying
+    @Transactional
+    @Query("update Author a set a.age = :age")
+    void updateAllAuthorsAges(int age);
+
     // select * from author where first_name = 'fname'
     List<Author> findAllByFirstName(String firstName);
     
@@ -23,4 +38,6 @@ public interface AuthorRepository extends JpaRepository<Author, Integer> {
 
     // select * from author where first_name in ('raid', 'thabet')
     List<Author> findAllByFirstNameInIgnoreCase(Collection<String> firstNames);
+
+
 }
