e26b3c3be90ccfd257f7d85088b27d5e097b8819
Raid Thabet <thabet.raid123@gmail.com>
2025-03-27 16:34:10 +0100

    Derived queries

--- src/main/java/com/raid/miniprojectjpa/repositories/AuthorRepository.java
+++ src/main/java/com/raid/miniprojectjpa/repositories/AuthorRepository.java
@@ -3,5 +3,24 @@ package com.raid.miniprojectjpa.repositories;
 import com.raid.miniprojectjpa.models.Author;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Collection;
+import java.util.List;
+
 public interface AuthorRepository extends JpaRepository<Author, Integer> {
+    // select * from author where first_name = 'fname'
+    List<Author> findAllByFirstName(String firstName);
+    
+    List<Author> findAllByFirstNameIgnoreCase(String firstName);
+
+    // select * from author where first_name like '%fna%'
+    List<Author> findAllByFirstNameContainingIgnoreCase(String firstName);
+
+    // select * from author where first_name like 'fna%'
+    List<Author> findAllByFirstNameStartsWithIgnoreCase(String firstName);
+
+    // select * from author where first_name like '%fna'
+    List<Author> findAllByFirstNameEndsWithIgnoreCase(String firstName);
+
+    // select * from author where first_name in ('raid', 'thabet')
+    List<Author> findAllByFirstNameInIgnoreCase(Collection<String> firstNames);
 }
